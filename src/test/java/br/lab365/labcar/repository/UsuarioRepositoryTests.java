656fc0a4bd0111cac0ed9fbb1b941bc2e01bf65b
ezeleis <67750812+ezeleis@users.noreply.github.com>
2023-10-01 15:56:54 -0500

    feature(M3S09):Ex2

---
+++ src/test/java/br/lab365/labcar/repository/UsuarioRepositoryTests.java
@@ -0,0 +1,72 @@
+package br.lab365.labcar.repository;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import br.lab365.labcar.model.UsuarioModel;
+import br.lab365.labcar.repository.UsuarioRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.dao.DataIntegrityViolationException;
+
+import java.util.Optional;
+
+public class UsuarioRepositoryTests {
+
+    @Mock
+    private UsuarioRepository usuarioRepository;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testSave() {
+        UsuarioModel newUser = new UsuarioModel();
+        newUser.setNome("John Doe");
+        newUser.setEmail("johndoe@example.com");
+        newUser.setSenha("password123");
+
+        when(usuarioRepository.save(newUser)).thenReturn(newUser);
+
+        UsuarioModel savedUser = usuarioRepository.save(newUser);
+
+        verify(usuarioRepository).save(newUser);
+
+        assertEquals(newUser, savedUser);
+    }
+
+    @Test
+    public void testFindById() {
+        UsuarioModel sampleUser = new UsuarioModel();
+        sampleUser.setId(1L);
+        sampleUser.setNome("Jane Doe");
+        sampleUser.setEmail("janedoe@example.com");
+        sampleUser.setSenha("password456");
+
+        when(usuarioRepository.findById(1L)).thenReturn(Optional.of(sampleUser));
+
+        Optional<UsuarioModel> user = usuarioRepository.findById(1L);
+
+        verify(usuarioRepository).findById(1L);
+
+        assertTrue(user.isPresent());
+        assertEquals("Jane Doe", user.get().getNome());
+        assertEquals("janedoe@example.com", user.get().getEmail());
+        assertEquals("password456", user.get().getSenha());
+    }
+
+    @Test
+    public void testSaveWithDuplicateId() {
+        UsuarioModel duplicateUser = new UsuarioModel();
+        duplicateUser.setId(1L);
+
+        when(usuarioRepository.save(duplicateUser)).thenThrow(DataIntegrityViolationException.class);
+
+        assertThrows(DataIntegrityViolationException.class, () -> usuarioRepository.save(duplicateUser));
+
+        verify(usuarioRepository).save(duplicateUser);
+    }
+}
